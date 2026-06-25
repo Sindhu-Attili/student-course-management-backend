19b06c251fd9dc70414dbd814280cd890f054753
Sindhu-Attili <sindhuattili14@gmail.com>
2026-06-25 11:54:19 +0530

    small change in middleware

--- config/settings.py
+++ config/settings.py
@@ -43,18 +43,18 @@ INSTALLED_APPS = [
     'education',
     'drf_spectacular',
     'corsheaders',
-    
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
