8f2b43269ebf462422ecedc1a06f58a18a097931
Sindhu-Attili <sindhuattili14@gmail.com>
2026-06-25 12:37:45 +0530

    exception handle was commented out

--- config/settings.py
+++ config/settings.py
@@ -133,8 +133,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_SCHEMA_CLASS':
         'drf_spectacular.openapi.AutoSchema',
-    'EXCEPTION_HANDLER':
-        'education.utils.custom_exception_handler',
+    #'EXCEPTION_HANDLER':
+        #'education.utils.custom_exception_handler',
 
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
